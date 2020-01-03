546b61fc30dbe3d357e110e70c6604f59897816e
LarryGF <larrywtf609@gmail.com>
2020-01-02 21:25:02 -0500

    Reformatted slack message

--- log_handler.py
+++ log_handler.py
@@ -8,5 +8,14 @@ with open(file) as file:
     data = file.read()
     data = data.split('\n')
     message = '\n'.join(data[-lines:])
+attachments = [
+    {
+        "pretext": file,
+        "title": "Log contents",
+        # "title_link": "https://groove.hq/path/to/ticket/1943",
+        "text": message,
+        "color": "#7CD197"
+    }
+]
 response = requests.post(url=slack_url, headers={
-                         'Content-type': 'application/json'}, json={'text': message})
+                         'Content-type': 'application/json'}, json={'attachments': attachments})
